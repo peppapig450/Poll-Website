6b27927a3b4b290195e8e80b0c6881ce4fcd0e41
peppapig450 <nbgenius1@gmail.com>
2024-07-13 16:29:19 -0700

    create a view and the template for it to display the results

--- pollsite/polls/views.py
+++ pollsite/polls/views.py
@@ -19,13 +19,14 @@ def detail(request, question_id):
     return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
-    return HttpResponse(f"You're looking at the results of question {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     
     try:
-        selected_choice = question.choice_set.get(pk=request.POST["choice"]) #
+        selected_choice = question.choice_set.get(pk=request.POST["choice"]) #type: ignore
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form
         return render(
