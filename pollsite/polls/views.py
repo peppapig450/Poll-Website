4893ab0db7b430eac87671412fc1f0088b66e168
peppapig450 <nbgenius1@gmail.com>
2024-07-13 16:50:14 -0700

    add a get_queryset method to DetailView as wel providing a sentinel guarding against future dates for details

--- pollsite/polls/views.py
+++ pollsite/polls/views.py
@@ -1,5 +1,4 @@
 from typing import Any
-
 from django.db.models import F
 from django.db.models.query import QuerySet
 from django.http import HttpResponseRedirect
@@ -10,18 +9,24 @@ from django.views import generic
 
 from .models import Choice, Question
 
-
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
     
     def get_queryset(self) -> QuerySet[Any]:
-        """Return the last five publisehd questions."""
+        """Return the last five published questions."""
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
     
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+    
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+        
     
 class ResultsView(generic.DetailView):
     model = Question
