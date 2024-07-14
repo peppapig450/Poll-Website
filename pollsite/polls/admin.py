e8d5879fb3c0164548c830d51cb88c653510464f
peppapig450 <nbgenius1@gmail.com>
2024-07-13 18:11:24 -0700

    use a StackedInline to display the choices along with the question when creating

--- pollsite/polls/admin.py
+++ pollsite/polls/admin.py
@@ -3,12 +3,18 @@ from django.contrib import admin
 from .models import Choice, Question
 
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
 # Register your models here.
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["question_text"]}),
-        ("Date information", {"fields": ["pub_date"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
